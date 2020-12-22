198c1b041a084d55a3598f409e3782e9ba215c7f
TomasRS <tomas_rs@hotmail.com>
2020-12-22 20:22:02 -0300

    json file to fetch event names instead of hardcoding

--- server.js
+++ server.js
@@ -4,49 +4,49 @@ const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
 const { userJoin, getCurrentUserById, userLeaves, getRoomUsers } = require('./utils/users');
+let values = require('./utils/values.json');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const botName = 'Chat Bot';
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
-io.on('connection', socket => {
-    socket.on('joinRoom', ({ username, room }) => {
+io.on(values.connectionEventName, socket => {
+    socket.on(values.joinRoomEventName, ({ username, room }) => {
         const user = userJoin(socket.id, username, room);
         socket.join(user.room);
 
         // Welcome current user
-        socket.emit('message', formatMessage(botName, `Welcome to the ${room} room!`));
+        socket.emit(values.messageEventName, formatMessage(values.chatBotName, `Welcome to the ${room} room!`));
 
         // Broadcast when a user connects
-        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.name} has joined the chat`));
+        socket.broadcast.to(user.room).emit(values.messageEventName, formatMessage(values.chatBotName, `${user.name} has joined the chat`));
 
         // Send users and room info
-        io.to(user.room).emit('roomUsers', {
+        io.to(user.room).emit(values.roomUsersEventName, {
             room: user.room,
             users: getRoomUsers(user.room)
         });
     });
 
     // Listen for a chat message
-    socket.on('chatMessage', (message) => {
+    socket.on(values.chatMessageEventName, (message) => {
         const user = getCurrentUserById(socket.id);
-        io.to(user.room).emit('message', formatMessage(user.name, message));
+        io.to(user.room).emit(values.messageEventName, formatMessage(user.name, message));
     });
 
     // Run when client disconnects
-    socket.on('disconnect', () => {
+    socket.on(values.disconnectEventName, () => {
         const user = userLeaves(socket.id);
         if(user){
             // Broadcast when a user disconnects
-            io.to(user.room).emit('message', formatMessage(botName, `${user.name} has left the chat`));
+            io.to(user.room).emit(values.messageEventName, formatMessage(values.chatBotName, `${user.name} has left the chat`));
 
             // Send users and room info
-            io.to(user.room).emit('roomUsers', {
+            io.to(user.room).emit(values.roomUsersEventName, {
                 room: user.room,
                 users: getRoomUsers(user.room)
             });
--- utils/messages.js
+++ utils/messages.js
@@ -1,11 +1,12 @@
 const moment = require('moment');
+let values = require('./values.json');
 
 function formatMessage(username, text){
     return {
         username,
         text,
-        time: moment().format('h:mm a'),
-        isChatBot: username === 'Chat Bot'
+        time: moment().format(values.timeFormat),
+        isChatBot: username === values.chatBotName
     }
 }
 
