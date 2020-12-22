0598218d08c3d403b724f219d259e699bccca2a7
TomasRS <tomas_rs@hotmail.com>
2020-12-22 11:20:53 -0300

    broadcast logging when user connects and disconnects

--- server.js
+++ server.js
@@ -12,7 +12,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Run when client connects
 io.on('connection', socket => {
-    console.log('New WebSocket connection...');
+
+    //Welcome current user
+    socket.emit('message', 'Welcome to TomChat!');
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //Run when client disconnects
+    socket.on('disconnect', () => {
+        //Broadcast when a user disconnects
+        io.emit('message', 'A user has left the chat');
+    });
 });
 
 const PORT = process.env.PORT || 3000;
