937c97bf9dfb71cad5ee570edb8ba19f5c1a21f5
TomasRS <tomas_rs@hotmail.com>
2021-01-18 18:29:45 -0300

    serverUrl property name fixed in prod file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  rootUrl: "https://chattingwebsite.azurewebsites.net",
+  serverUrl: "https://chattingwebsite.azurewebsites.net",
   timeFormat: "h:mm a",
   localStorageUsernameKey: "webchat-username",
   roomNames: [ "General", "Games", "College" ],
