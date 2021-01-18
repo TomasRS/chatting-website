edecff2368a2d84b208400222552e34146de4030
TomasRS <tomas_rs@hotmail.com>
2021-01-18 18:03:41 -0300

    removing port from environment.prod file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  rootUrl: "https://chattingwebsite.azurewebsites.net/3000",
+  rootUrl: "https://chattingwebsite.azurewebsites.net",
   timeFormat: "h:mm a",
   localStorageUsernameKey: "webchat-username",
   roomNames: [ "General", "Games", "College" ],
